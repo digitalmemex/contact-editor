0ffd7ee6fc9fc7393988e74ff1342a62d50ee6c9
dgf <deep@dagnu.de>
2014-12-28 14:34:31 +0100

    reuse DOM references

--- contacts.js
+++ contacts.js
@@ -3,9 +3,11 @@
 $(function() { // jQuery ready => start up
   var dmc = new RESTClient('/core'),
       institutions = dmc.get_topics('dm4.contacts.institution', true, true),
+      $institutions = $('#institutions').empty(),
       people = dmc.get_topics('dm4.contacts.person', true, true),
+      $people = $('#people').empty(),
       renderContact = function ($body, c) { $body.append($('<tr>').append($('<td>').text(c.value))) };
-  $.each(institutions.items, function (i, c) { renderContact($('#institutions').empty(), c) });
-  $.each(people.items, function (i, c) { renderContact($('#people').empty(), c) });
+  $.each(institutions.items, function (i, c) { renderContact($institutions, c) });
+  $.each(people.items, function (i, c) { renderContact($people, c) });
 });
 
